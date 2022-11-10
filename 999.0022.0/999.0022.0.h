c0d7681b5f47e0c7b575bc4c465673d12d0b04da
Cristiano Diel <cristianodiel@gmail.com>
2022-11-10 11:47:13 -0300

    add uCAN message 0022

--- 999.0022.0/999.0022.0.h
+++ 999.0022.0/999.0022.0.h
@@ -2,3 +2,21 @@ void ConfigMCU();
 void ECAN_Initialize();
 void CAN_transmit(*tempCanMsg);
 void CAN_receive(*tempCanMsg);
+typedef union
+{
+    struct
+    {
+        uint8_t idType;
+        uint32_t id;
+        uint8_t dlc;
+        uint8_t data0;
+        uint8_t data1;
+        uint8_t data2;
+        uint8_t data3;
+        uint8_t data4;
+        uint8_t data5;
+        uint8_t data6;
+        uint8_t data7;
+    } frame;
+    uint8_t array[14];
+} uCAN_MSG;
--- 999.0022.0/main.c
+++ 999.0022.0/main.c
@@ -6,18 +6,29 @@ int main()
 {
     /*Inicializar o microcontrolador (apenas chamando a função ConfigMCU);*/
     ConfigMCU();
+
     /*Confirmar a presença (via CAN) do item 999.0020.0, se o item 999.0020.0 estiver presente
     nada deverá acontecer, mas se ele não responder, o programa para de rodar até que o item
     seja conectado.*/
     ECAN_Initialize(); // inicializa o protocolo CAN
-    while (CAN_receive(*tempCanMsg)){
-        //programa funciona normalmente
 
-    }else{
-        //programa para de rodar até que o item seja conectado
+    uCAN_MSG rxMessage;
+    while (CAN_receive(&rxMessage)){//programa funciona normalmente
+        /*Confirmando que o sistema foi ligado corretamente, o programa deverá enviar a cada 1
+        segundo uma mensagem (via CAN) “perguntando” ao item 999.0020.0 se ele está presente;*/
+        função_timer(1s){
+            //item 999.0020.0, a câmera está ligada?
+            CAN_transmit(uCAN_MSG *tempCanMsg);
+            CAN_receive(uCAN_MSG *tempCanMsg);
+        }
+
+    }else{/*enquanto a função retornar false,
+        o programa para de rodar até que o item seja conectado*/
+        //printf("Mensagem de debug: Aguardando o item 999.0020.0 ser conectado.\n");
+        void ISR() iv 0x0018 ics ICS_AUTO {/*insira o código aqui*/};
+
     };
-    /*Confirmando que o sistema foi ligado corretamente, o programa deverá enviar a cada 1
-    segundo uma mensagem (via CAN) “perguntando” ao item 999.0020.0 se ele está presente;*/
+
     /*Passado mais 1 segundo, se o item 999.0020.0 estiver presente nada deverá acontecer, mas se
     ele não responder, o programa para de rodar até que o item seja re-conectado*/
 
